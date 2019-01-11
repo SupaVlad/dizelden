f5627676858c528aed5503737cfb8e31ae76463e
Владислав Бородин <vb@rocketstation.ru>
2019-01-11 16:54:39 +0300

    123

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,30 @@
 import '../styles/index.scss';
+window.$ = window.jQuery = require('jquery');
+$(document).ready(function() {
+
+  function SelectContent (element) {
+    var doc = document
+      , text = doc.getElementById(element)
+      , range, selection
+    ;
+    if (doc.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+    } else if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+    }
+    document.execCommand('Copy');
+  }
+
+  $(".copy").click(function () {
+    SelectContent($(this).attr('title'));
+  });
+});
 
 console.log('webpack starterkit');
